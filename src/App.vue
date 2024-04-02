29fc5156e9d7b667436ef8b3dd113064949755f1
luckzp <245527922@qq.com>
2024-04-02 22:40:59 +0800

    props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="app">hello</div>
+  <div class="app">  <JobList :jobs="jobs"/></div>
 </template>
 
 <script setup lang="ts">
-
+import {ref} from'vue'
+import Job from './types/Job'
+import JobList from '@/components/JobList.vue'
+const jobs = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+])
 </script>
 
 <style>
